e6e1b371226b24d25f4d7fb64e93a8261523eca3
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2025-08-20 02:31:26 -0300

    feat: created Movie entity module

---
+++ src/modules/movie/entities/Movie.ts
@@ -0,0 +1,98 @@
+import cuid from 'cuid';
+import { Replace } from 'src/utils/replace';
+
+interface MovieSchema {
+  id: string;
+  title: string;
+  description: string | null;
+  releaseDate: Date;
+  rating: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+  stills: [];
+}
+
+export class Movie {
+  private readonly props: MovieSchema;
+  private readonly _id: string;
+
+  constructor(
+    props: Replace<
+      MovieSchema,
+      {
+        createdAt?: Date;
+        updatedAt?: Date;
+        description?: string | null;
+        rating?: number | null;
+        stills?: [];
+      }
+    >,
+    id?: string,
+  ) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt || new Date(),
+      updatedAt: props.updatedAt || new Date(),
+      description: props.description ?? null,
+      rating: props.rating ?? null,
+      stills: props.stills ?? [],
+    };
+    this._id = id || cuid();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get title(): string {
+    return this.props.title;
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+  }
+
+  get description(): string | null {
+    return this.props.description;
+  }
+
+  set description(description: string | null) {
+    this.props.description = description;
+  }
+
+  get releaseDate(): Date {
+    return this.props.releaseDate;
+  }
+
+  set releaseDate(releaseDate: Date) {
+    this.props.releaseDate = releaseDate;
+  }
+
+  get rating(): number | null {
+    return this.props.rating;
+  }
+
+  set rating(rating: number | null) {
+    this.props.rating = rating;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+
+  set updatedAt(updatedAt: Date) {
+    this.props.updatedAt = updatedAt;
+  }
+
+  get stills(): [] {
+    return this.props.stills;
+  }
+
+  set stills(stills: []) {
+    this.props.stills = stills;
+  }
+}
