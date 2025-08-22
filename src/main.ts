8ecbc86b348dbe93b277d1002bf860fc847f2a73
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2025-08-22 02:24:50 -0300

    feat: move Swagger setup logic to a separate module for better organization

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('framegrab')
-    .setDescription('framegrab is a tool for extracting frames from videos.')
-    .setVersion('1.0.0')
-    .addBearerAuth()
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  setupSwagger(app);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
+
 void bootstrap();
---
+++ src/swagger.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Framegrab API')
+    .setDescription('Framegrab is a tool for extracting frames from movies.')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+}
