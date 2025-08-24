8ffcb94226110f210211082ca68983114255634e
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2025-08-24 04:24:16 -0300

    fix: removed ignored files

--- src/modules/movie/repositories/MovieRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Movie } from '../entities/Movie';
-
-export abstract class MovieRepository {
-  abstract create(movie: Movie): Promise<void>;
-  abstract getAll(page: number, limit: number): Promise<Movie[]>;
-  abstract delete(id: string): Promise<void>;
-  abstract findById(id: string): Promise<Movie | null>;
-  abstract update(movie: Movie): Promise<void>;
-}
--- src/modules/still/repositories/StillRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Still } from '../entities/Still';
-
-export abstract class StillRepository {
-  abstract create(still: Still): Promise<void>;
-  abstract createMany(stills: Still[]): Promise<void>;
-  abstract delete(id: string): Promise<void>;
-  abstract findById(id: string): Promise<Still | null>;
-}
